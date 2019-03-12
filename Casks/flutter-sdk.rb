2901e64b7ea3d9512eeefea1e52cfbf2278c2ed3
Coel <coeligenai@gmail.com>
2019-03-12 21:39:52 +0300

    Create flutter-sdk.rb

---
+++ Casks/flutter-sdk.rb
@@ -0,0 +1,28 @@
+cask 'flutter-sdk' do
+
+  name "Flutter"
+  homepage "https://www.flutter.dev"
+
+  version "1.2.1"
+  sha256 "74ac8397ea29720f116980ea00cf60c34430be1f64489b407f7cf95553babbef"
+  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
+
+  option "with-android-sdk", "Also installs Android SDK via Brew"
+
+  depends_on cask: "android-sdk" => :optional
+  depends_on cask: "visual-studio-code"
+  depends_on :arch => :x86_64
+
+  binary "#{staged_path}/flutter/bin/flutter"
+
+  postflight do
+    safe_system "#{staged_path}/flutter/bin/flutter doctor"
+  end
+
+  caveats do
+    <<~EOS
+      You may want to add to your profile:
+        'source <(flutter bash-completion)'
+    EOS
+  end
+end
