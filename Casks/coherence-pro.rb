319d04d0deab8e3ab9660d1f31c7d1a08262f09d
Coel <coeligenai@gmail.com>
2019-03-19 02:18:59 +0300

    no message

---
+++ Casks/coherence-pro.rb
@@ -0,0 +1,11 @@
+cask 'coherence-pro' do
+  version :latest
+  sha256 '16f0fd25bcad512781ba469c7992fbecbb065559dd61fd190e7383efcdac856b'
+
+  # release notes - https://www.bzgapps.com/coherenceprodownloads
+  url 'https://dl.devmate.com/com.BZG.CoherencePro/CoherencePro.zip'
+  name 'Coherence Pro'
+  homepage 'https://www.bzgapps.com/coherencepro'
+
+  app 'Coherence Pro.app'
+end
