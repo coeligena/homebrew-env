2951b52b74a0696393d1cecbf2adf321f6130289
Coel <coeligenai@gmail.com>
2019-03-18 18:16:40 +0300

    no message

--- Casks/apache-cayenne-modeler-beta.rb
+++ Casks/apache-cayenne-modeler-beta.rb
@@ -1,11 +1,11 @@
 cask 'apache-cayenne-modeler-dev' do
-  version '4.1.M1'
+  version '4.1.B1'
   sha256 '13382c4ce1d1ac74cef24954f07e16270efa88eadb6b44a3e1af49beaa360551'
 
-  url "https://www-eu.apache.org/dist/cayenne/#{version}/cayenne-#{version}-macosx.dmg"
+  url "https://www.apache.org/dist/cayenne/#{version}/cayenne-#{version}-macosx.dmg"
   name 'Apache Cayenne Modeler (Development)'
   homepage 'https://cayenne.apache.org/download.html'
 
-  suite "cayenne-#{version}-macosx", :target => 'CayenneModeler'
+  suite "cayenne-#{version}-macosx", :target => 'CayenneModelerDev'
   app 'CayenneModeler.app', :target => "#{appdir}/CayenneModelerDev/CayenneModeler.app"
 end
