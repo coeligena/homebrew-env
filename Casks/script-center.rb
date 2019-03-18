460c3b9598c1938d3a7f454714e87808c4e34e16
Coel <coeligenai@gmail.com>
2019-03-18 23:57:03 +0300

    no message

---
+++ Casks/script-center.rb
@@ -0,0 +1,10 @@
+cask 'script-center' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://gngrwzrd.com/Script%20Center.zip'
+  name 'Script Center'
+  homepage 'https://gngrwzrd.com/script-center/'
+
+  app 'Script Center.app'
+end
