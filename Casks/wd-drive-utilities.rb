53a7aa324ecc12d3158218395633480e260b399d
Coel <coeligenai@gmail.com>
2019-04-03 15:09:42 +0300

    no message

---
+++ Casks/wd-drive-utilities.rb
@@ -0,0 +1,15 @@
+cask 'wd-drive-utilities' do
+  version '2.1.1.100'
+  sha256 '1abbdf3f89c9bbb0b28e72ffa92f03c3355b4dad783cf68d16a8928abd41cb13'
+
+  url 'http://downloads.wdc.com/wdapp/WD_Utilities_Standalone_Installer_Mac_2_1_1_100.zip'
+  name 'WD Drive Utilities for Mac'
+  homepage 'https://support.wdc.com/downloads.aspx?p=157'
+
+  installer manual: 'WD Drive Utilities Installer.app'
+
+  uninstall script: {
+                      executable: '/Applications/WD Drive Utilities Uninstaller.app/Contents/MacOS/WD WD Drive Utilities Installer',
+                      sudo:       true,
+                    }
+end
