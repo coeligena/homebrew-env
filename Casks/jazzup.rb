3c6c7f9bf0dd63934a588f5c6fad4e47801067d4
Coel <coeligenai@gmail.com>
2019-11-13 23:35:25 +0300

    no message

---
+++ Casks/jazzup.rb
@@ -0,0 +1,10 @@
+cask 'jazzup' do
+  version :latest
+  sha256 '23139ee856d79b6e78ad3badee82f20b1972f29ffb930a4dd7884d401311a3f8'
+
+  url "https://www.irradiatedsoftware.com/download/JazzUp.zip"
+  name 'JazzUp'
+  homepage 'http://www.irradiatedsoftware.com/labs/'
+
+  app 'JazzUp.app'
+end
