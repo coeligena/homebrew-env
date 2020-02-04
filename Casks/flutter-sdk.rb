b405b1808e84de4208ab3c83d42dbc19c6a23259
Coel <coeligenai@gmail.com>
2020-02-04 16:09:11 +0300

    no message

--- Casks/flutter-sdk.rb
+++ Casks/flutter-sdk.rb
@@ -1,7 +1,8 @@
 cask 'flutter-sdk' do
   version 'latest'
+  sha256 'f4d9213c8c5777aed05e2c0dee3261b2baa21a11d27e9f28c76879ee7164b71f'
 
-  url 'https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.9.1+hotfix.4-stable.zip'
+  url 'https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.12.13+hotfix.7-stable.zip'
 
   depends_on formula: 'usbmuxd'
   depends_on formula: 'libimobiledevice'
