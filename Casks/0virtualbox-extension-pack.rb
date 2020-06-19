d0d63f3871bcf8914d55b5a2631761d3a0d4aa03
Coel <coeligenai@gmail.com>
2020-06-19 19:24:32 +0300

    no message

---
+++ Casks/0virtualbox-extension-pack.rb
@@ -0,0 +1,41 @@
+cask '0virtualbox-extension-pack' do
+  version '6.1.10'
+  sha256 '03067f27f4da07c5d0fdafc56d27e3ea23a60682b333b2a1010fb74ef9a40c28'
+
+  url "https://download.virtualbox.org/virtualbox/#{version}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack"
+  appcast 'https://download.virtualbox.org/virtualbox/LATEST.TXT'
+  name 'Oracle VirtualBox Extension Pack'
+  homepage 'https://www.virtualbox.org/'
+
+  conflicts_with cask: 'virtualbox-extension-pack-beta'
+  depends_on cask: 'virtualbox'
+  container type: :naked
+
+  stage_only true
+
+  postflight do
+    system_command '/usr/local/bin/VBoxManage',
+                   args:  [
+                            'extpack', 'install',
+                            '--replace', "#{staged_path}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack",
+                            '--accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb'
+                          ],
+                   input: 'y',
+                   sudo:  true
+  end
+
+  uninstall_postflight do
+    next unless File.exist?('/usr/local/bin/VBoxManage')
+
+    system_command '/usr/local/bin/VBoxManage',
+                   args: [
+                           'extpack', 'uninstall',
+                           'Oracle VM VirtualBox Extension Pack'
+                         ],
+                   sudo: true
+  end
+
+  caveats do
+    license 'https://www.virtualbox.org/wiki/VirtualBox_PUEL'
+  end
+end
